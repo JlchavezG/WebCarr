a47d0651c7fcfa333049c2f06671d9560676d653
IscJlchavezG <joseluis@iscjlchavezg.mx>
2023-12-02 13:21:48 -0600

    consultas y base de datos carrito

--- catalogo.php
+++ catalogo.php
@@ -1,9 +1,6 @@
 <?php
 include "include/conect.php";
 include "include/consultas.php";
-$Bproducto = "SELECT * FROM Productos ORDER BY Id_Producto";
-$E = $ConnetBd->query($Bproducto);
-
 
 ?>
 <!DOCTYPE html>
@@ -17,44 +14,48 @@ $E = $ConnetBd->query($Bproducto);
 </head>
 
 <body>
+
     <div class="container">
-        <div class="row mt-2">
-            <div class="col-lg-12 col-md-12 col-sm-12">
-                <h5 class="text-center">Ver todas las categorias del carrito</h5>
-            </div>
-            <div class="row">
-                <span>Total de productos: <?php echo $NProductos; ?></span>
-            </div>
-            <div class="row mt-2">
-            <?php while($row = $E->fetch_assoc()){ ?>
-                <div class="card shadow mb3" style="max-width:540px">
-                    <div class="row no-gutters">
-                        <div class="col-sm-4 col-sm-4 col-lg-4">
-                            <img src="img/productos/<?php echo $row['ImagenProducto']; ?>" alt="Figura de accion" class="card-img" style="width:150px;">
+        <div class="row">
+            <span class="text-center mt-3 display-6">Carrito de Compra</span>
+        </div>
+        <div class="row mt-2 text-end">
+             <span class="text-end text-success"> Numero de Productos: <?php echo $NProductos; ?> </span>
+        </div>
+        <?php while($row = $EProductos->fetch_assoc()){?>
+        <div class="row mt-3 d-flex justify-content-center">
+            <div class="col-sm-6 col-md-6 col-lg-6">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-sm-4 col-md-4 col-lg-4">
+                            <img src="img/productos/<?php echo $row['ImagenProducto']; ?>" class="card-img" alt="" style="width:150px;">
                         </div>
                         <div class="col-sm-8 col-md-8 col-lg-8">
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $row['NombreProducto']; ?></h5>
-                                <p class="card-text">Descripción: <?php echo $row['Descripcion']; ?></p>
-                                <p class="card-text"><small class="text-muted">Precio: MNX <?php echo $row['Precio']; ?></small></p>
+                            <div class="row">
+                                <h2 class="fs-5 mt-3"><?php echo $row['NombreProducto']; ?></h2>
                             </div>
-                            <div class="col-sm-12 col-md-12 col-lg-12">
-                                  <form mane="productos" action="" method="Post">
-                                      <input type="hidden" name="id" placeholder="Id_del producto">
-                                      <input type="hidden" name="nombre" placeholder="Nombre del producto">
-                                      <input type="hidden" name="precio" placeholder="precio del producto">
-                                      <input type="hidden" name="numero" placeholder="Numero de producto"> 
-                                      <div>
-                                        <button type="submit" name="btn_carrito" value="Agregar a carrito" class="btn btn-sm btn-success">Agregar al carrito</button>
-                                      </div>
-                                  </form>
+                            <div class="row">
+                                <p class="fs-6 text-muted">Descripción:<?php echo $row['Descripcion']; ?></p>
+                                <p class="text-danger">Precio: MNX <?php echo$row['Precio']; ?></p>
+                            </div>
+                            <div class="row mt-2 py-2">
+                                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                                    <input type="hidden" name="IdProduct" id="IdProduct" value="<?php echo $row['Id_Producto']; ?>">
+                                    <input type="hidden" name="NameProduct" id="NameProduct" value="<?php echo $row['NombreProducto']; ?>">
+                                    <input type="hidden" name="Descripcion" id="Descripcion" value="<?php echo $row['Descripcion']; ?>">
+                                    <input type="hidden" name="Precio" id="Precio" value="<?php echo $row['Precio']; ?>">
+                                    <input type="number" name="Cantidad" id="Cantidad" min="1" max="10" class="form-control" placeholder="selecciona la cantidad">
+                                    <div class="row mt-2">
+                                        <input type="submit" value="Enviar al carrito" class="btn btn-sm btn-success" name="btn_carrito">
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <?php } ?>
         </div>
+        <?php } ?>
     </div>
     <script src="js/bootstrap.min.js"></script>
 </body>
